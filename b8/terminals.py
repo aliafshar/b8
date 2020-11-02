8e2c81266ffd0cbb8ac3f163158fc05186de4afb
Ali Afshar <aafshar@gmail.com>
2020-11-02 20:19:17 +0000

    Clean up terminal watchers.

--- b8/terminals.py
+++ b8/terminals.py
@@ -144,6 +144,7 @@ class Terminals(Gtk.Notebook, ui.MenuHandlerMixin):
     print('shutdown', self.get_children())
     for child in self.get_children():
       child.term.disconnect(child.exited_handler)
+      child.cwd_discovery.stop()
       if child.pid > 0:
         os.kill(child.pid, 15)
 
